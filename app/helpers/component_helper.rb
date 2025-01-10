6b637d2859530435d2ced5c1366cec752dc9f9d6
Ryan Bosinger <rbosinger@gmail.com>
2025-01-10 00:04:51 -0800

    Cherry pick 659e82e9fcd8b34738cc82ef6150c656df90a56e

--- app/helpers/component_helper.rb
+++ app/helpers/component_helper.rb
@@ -1,6 +1,20 @@
+#!/usr/bin/ruby
 module ComponentHelper
-  def component(path, options = {}, &block)
+  def component(path, locals = {}, &block)
     full_path = Pathname.new("components") / path
-    render(full_path.to_s, options)
+
+    if block
+      # Render the passed block within the current context and
+      # store it in the `content` variable
+      content = capture do
+        block.call
+      end
+
+      # Call render but pass it a new block that yields just
+      # the contents of the `content` variable
+      render(full_path.to_s, locals) { content }
+    else
+      render(full_path.to_s, locals)
+    end
   end
 end
